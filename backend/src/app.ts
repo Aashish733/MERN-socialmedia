e896ab5a9aa3170909d20529eaa7b6f13c67a971
Aashish909 <aashish24062000@gmail.com>
2026-03-21 15:10:27 +0530

    implement backend structure with user and post models, add test route and controllers, and configure middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,22 @@
+import cookieParser from 'cookie-parser';
 import express from 'express'
+import cors from "cors";
+import testRoute from "./routes/testRoute.js";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+//routes
+app.use("/api/v1/test", testRoute);
+
+
 
 export default app;
---
+++ backend/src/models/post.model.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+
+const postSchema = new mongoose.Schema(
+  {
+   content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      required: false,
+    },
+    video: {
+      required: false,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+ 
+  {
+    timestamps: true,
+  }
+);
+
+
+
+export const Post = mongoose.model("Post", postSchema);
